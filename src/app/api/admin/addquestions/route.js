a54dcd1676e65de3bd2644642f0fde7aab09e532
patiladitya77 <adpatil587@gmail.com>
2025-09-15 13:06:01 +0530

    API: addquestions

---
+++ src/app/api/admin/addquestions/route.js
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+
+export async function POST(req) {
+    try {
+        const body = await req.json();
+        const { roomId, question, options, correctAnswer } = body;
+
+
+        if (!roomId || !question || !options || !correctAnswer) {
+            return NextResponse.json({ error: "Missing fields" }, { status: 400 });
+        }
+
+        const newQuestion = await prisma.question.create({
+            data: {
+                question,
+                options,
+                correctAnswer,
+                room: {
+                    connect: { id: roomId },
+                },
+            },
+        });
+
+        return NextResponse.json(newQuestion, { status: 201 });
+    } catch (err) {
+        console.error("AddQuestion API Error:", err);
+        return NextResponse.json({ error: err.message }, { status: 500 });
+    }
+}
